5a222162326bee2b88e2db446c479028cd8e3893
Pavel Miatlitski <pmetlitsky@smartasset.com>
2023-05-31 14:50:20 +0300

    update fav icon

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,9 @@
 function App() {
-    return <h1 className="App">Images Gallery</h1>;
+    return (
+        <section>
+            <h1 className="App">Images Gallery</h1> <p>Intial commit</p>
+        </section>
+    );
 }
 
 export default App;
